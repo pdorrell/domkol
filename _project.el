07cd2fe8aa5d67f63566163e2c88447a0712e548
Philip Dorrell <philip@Cangallo-Ubuntu.(none)>
2013-04-15 13:59:00 +1200

    run-project-command

--- _project.el
+++ _project.el
@@ -4,5 +4,7 @@
  `( (:ruby-executable ,*ruby-1.9-executable*)
     (:search-extensions (".js" ".html"))
     (:compile-command ,"rake")
+    (:main-html-file "main.html")
+    (:run-project-command (open-file-in-web-browser ,(project-file :main-html-file)))
     (:build-function project-compile-with-command)
     ) )
