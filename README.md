40f2f815c9ea55bf11dd06a5ccc04ae7206a1d6a
Philip Dorrell <philip@Cangallo-Ubuntu.(none)>
2013-04-25 12:26:13 +1200

    add lgpl 3 license

--- README.md
+++ README.md
@@ -4,3 +4,7 @@ DomKol
 By Philip Dorrell
 
 For current details, see the DomKol homepage at http://thinkinghard.com/domkol/main.html.
+
+This software is licensed under the [http://thinkinghard.com/domkol/LICENSE.txt](GNU Lesser General Public License version 3).
+
+
--- domkol.js
+++ domkol.js
@@ -1,5 +1,21 @@
 /** 
-    Domkol. 
+    This file is part of Domkol.
+
+    Domkol is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Domkol is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Domkol.  If not, see <http://www.gnu.org/licenses/>.
+
+    Domkol
+    ====== 
     
     Complex function visualisation with domain colouring, interactive "domain circle" and dragging of
     polynomial function zeroes.
