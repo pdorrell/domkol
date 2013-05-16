1e561906daeee9bbd795f8cb39dcf335f82e0344
Philip Dorrell <philip@Cangallo-Ubuntu.(none)>
2013-05-16 15:55:00 +1200

    quintic polynomial

---
+++ quintic-polynomial.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+  
+  var controlDialogElement = new DOMKOL.ControlDialogElement($("#control-dialog"));
+  
+  var controlDialog = new DOMKOL.ControlDialog(controlDialogElement);
+  
+  controlDialog.addFunctionInstructions("Drag the blue numbers to change the zeroes of the quintic polynomial " + 
+                                        "(initially they are all located on the origin).");
+
+  var zeroes = [[0, 0], [0, 0], [0, 0], [0, 0], [0, 0]];
+  var polynomialFunction = new DOMKOL.PolynomialFunction(zeroes);
+
+  var explorerView = DOMKOL.createExplorerView($("#domkol"), polynomialFunction, controlDialog.values, 
+                                               300, [350, 350], [700, 700], 250);
+  
+  var numZeroes = zeroes.length;
+  for (i=numZeroes-1; i >= 0; i--) { // create in reverse order, because last one created is first one to be dragged
+    var zeroHandle = explorerView.createNumberHandle(i, zeroes[i]);
+    $(zeroHandle).on("numberChanged", 
+                     function(event, index, number, changing) {
+                       polynomialFunction.updateZero(index, number, changing);
+                     });
+  }
+  
+  controlDialog.connect(explorerView);
+});
