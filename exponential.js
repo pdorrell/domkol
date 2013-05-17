b7dc8f4fb4882ff8375ed1551881092c271ba36c
Philip Dorrell <philip@Cangallo-Ubuntu.(none)>
2013-05-17 16:20:15 +1200

    comments in exponential.js

--- exponential.js
+++ exponential.js
@@ -1,9 +1,12 @@
 $(document).ready(function(){
   
+  /** Object representing the control Dialog DOM Tree within the specified div element. */
   var controlDialogElement = new DOMKOL.ControlDialogElement($("#control-dialog"));
   
+  /** The Control Dialog itself (as an object which can be connected to the Complex Plane & graph view)*/
   var controlDialog = new DOMKOL.ControlDialog(controlDialogElement);
   
+  /** Complex exponential function, where complex numbers are represented as an array of two floats */
   function exponential(z) {
     var realFactor = Math.exp(z[0]);
     var cos = Math.cos(z[1]);
@@ -11,10 +14,18 @@ $(document).ready(function(){
     return [realFactor*cos, realFactor*sin];
   }
   
+  /** Object representing the exponential function */
   var exponentialFunction = new DOMKOL.ComplexFunction(exponential, "exp(x)");
 
-  var explorerView = DOMKOL.createExplorerView($("#domkol"), exponentialFunction, controlDialog.values, 
-                                               80, [250, 350], [800, 800], 150);
+  /** Explorer view - consisting of complex plane, coordinates and circular graph */
+  var explorerView = DOMKOL.createExplorerView($("#domkol"), // div element in which to insert the complex plane view
+                                               exponentialFunction, // the exponential function
+                                               controlDialog.values, // initial values for the explorer view settings
+                                               80, // pixels per unit
+                                               [250, 350], // pixel location of complex origin
+                                               [800, 800], // width * height in pixels of complex plane
+                                               150); // initial radius in pixels of the circle
   
+  /** Connect the control dialog so that it actually controls the explorer view */
   controlDialog.connect(explorerView);
 });
