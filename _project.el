4360ac47decc5d1e007355e19ee3566b9cf426b4
Philip <philip@thinkinghard.com>
2025-07-31 14:23:11 +1200

    update to be a javascript project

--- _project.el
+++ _project.el
@@ -1,11 +1,13 @@
 ;; Project values
 
 (load-this-project
- `( (:ruby-executable ,*ruby-1.9-executable*)
-    (:search-extensions (".js" ".html" ".css" ".scss"))
-    (:compile-command ,"rake")
-    (:main-html-file "main.html")
-    (:run-project-command (open-file-in-web-browser (project-file :main-html-file)))
-    (:build-function project-compile-with-command)
-    (:sass-watch-src-output-argument ,(concat (project-base-directory)))
-    ) )
+ `( 
+   (:project-type javascript)
+   (:ruby-executable "ruby")
+   (:compile-command "rake")
+   (:main-html-file "main.html")
+   (:run-project-command (open-file-in-web-browser (project-file :main-html-file)))
+   (:build-function project-compile-with-command)
+   (:sass-watch-src-output-argument "."))
+   ) )
+
